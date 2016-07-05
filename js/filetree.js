9de88e16f18e811b13bfab5fc11995bc301ebfa2
Anna Clawson <seadeer@gmail.com>
2016-07-05 13:35:04 -0700

    no message

--- js/filetree.js
+++ js/filetree.js
@@ -1,7 +1,10 @@
 var svgObject = document.getElementById("graphic")
+alert("Hey!")
 var fs = require('fs')
-svgObject.onload = function(){
-    var mySvg = svgObject.contentDocument.getElementsByTagName('svg')[0]
-    var two = new Two().appendTo(mySvg)
-    
+var two = new Two().appendTo(svgObject)
+
+function getDirContent(){
+    console.log("In here");
+    var dircontent = fs.readdirSync('/')
+    console.log(dircontent)
 }
