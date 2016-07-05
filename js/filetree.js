6b815ac3c45b7e64242b498ebdc3b72aeef0eeda
Anna Clawson <seadeer@gmail.com>
2016-07-05 15:06:40 -0700

    server serves up home directory structure upon get request

--- js/filetree.js
+++ js/filetree.js
@@ -1,10 +1,7 @@
 var svgObject = document.getElementById("graphic")
 alert("Hey!")
-var fs = require('fs')
 var two = new Two().appendTo(svgObject)
-
 function getDirContent(){
     console.log("In here");
-    var dircontent = fs.readdirSync('/')
-    console.log(dircontent)
 }
+
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
-// var bodyParser = require('body-parser');
+var fs = require('fs');
+var bodyParser = require('body-parser');
 var session = require('express-session');
 var path = require('path');
 var app = express();
@@ -8,13 +9,19 @@ var server = app.listen(8000, function(){
     console.log("listening on port 8000\nPress Ctrl-C to stop.");
 });
 var io = require('socket.io').listen(server);
-app.use(express.static(root));
+// app.use(express.static(root));
 app.use(session({secret: 'keyboard cat', cookie: { maxAge: 60000}, resave: true, saveUninitialized: true}));
-// app.use(bodyParser.urlencoded({extended:true}));
-app.set('views', path.join(__dirname, './views'));
-app.set('view engine', 'ejs');
 
 
+app.get('/', function(req, res){
+    var mydir = fs.readdirSync(getUserHome());
+    console.log(mydir);
+    res.json(mydir);
+});
+
+function getUserHome() {
+  return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+}
 
 
 
