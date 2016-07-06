978e317b7e0d74ca20712cb921839c13ede482d3
Anna Clawson <seadeer@gmail.com>
2016-07-05 17:20:48 -0700

    resize svg canvas each time a new text element is added

--- js/filetree.js
+++ js/filetree.js
@@ -2,20 +2,33 @@ $(document).ready(function(){
     var svgObject = document.getElementById("graphic");
     alert("Hey!");
     var two = new Two().appendTo(svgObject);
+    two.style = "overflow-y:auto;"
+    
+    function getJsonData(){
+        $.getJSON("/dirstruct", function(data){
+            console.log(data);
+            var x = 100
+            var y = 20
+            for(i in data){
+                var text = new Two.Text(data[i], x, y)
+                y+=20
+                text.stroke = '#FF8000'
+                text.id = data[i]
+                two.add(text)
+                two.height += 30;
+                two.update()
+            }
+        });
+    }
 
-    $.getJSON("/dirstruct", function(data){
-        console.log(data);
-        var x = 20
-        var y = 20
-        for(i in data){
-            var text = new Two.Text(data[i], x, y)
-            y+=20
-            text.stroke = '#FF8000'
-            two.add(text)
-            two.update()
-        }
-    });
+    getJsonData()
+
+    
 
 });
+    
+    
+
+
 
 
