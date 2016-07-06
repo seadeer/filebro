f440362fa645f78925eb948b7b4dec652ff0e9f8
Anna Clawson <seadeer@gmail.com>
2016-07-06 16:24:48 -0700

    Switched to using sockets instead of http requests; can navigate forward in the file tree.

--- js/filetree.js
+++ js/filetree.js
@@ -1,58 +1,46 @@
 $(document).ready(function(){
+    var path = "";
     var svgObject = document.getElementById("graphic");
-    alert("Hey!");
     var two = new Two().appendTo(svgObject);
     two.style = "overflow-y:auto;";
     
-    function getJsonData(path){
-        console.log("my path now:", path)
-        $.getJSON("/dirstruct/" + path, function(data){
-            two.clear()
-            var x = 100;
-            var y = 20;
-            for(var i in data){
-                var text = new Two.Text(data[i], x, y);
-                y+=20;
-                text.stroke = '#FF8000';
-                text.id = i;
-                $('body').on('click', "#"+text.id, function(){
-                    console.log("click!");
-                    var filename = $(this).text() 
-                    path = path + filename + '/'
-                    getFileInfo(path, function(data){
-                        console.log("Path: ", path);
-                        if(!data["message"]){
-                            console.log("it's a folder")
-                            getJsonData(path)
-                        }
-                        else{
-                            console.log(data["message"])
-                        }
-                    });
+    var socket = io.connect('localhost:8000');
+    socket.on("send_file_tree", function(data){
+        console.log("read file tree", data.filetree)
+        $('#container').off('click')
+        two.clear()
+        var x = 100;
+        var y = 20;
+        for(var i in data.filetree){
+            var text = new Two.Text(data.filetree[i], x, y);
+            y+=20;
+            text.stroke = '#FF8000';
+            text.id = i;
+//Add event listener to each element
+            $('#container').on('click', "#"+text.id, function(){
+                    console.log("click!", $(this).text());
+                    ReadFileInfo($(this).text())
                 });
-                two.add(text)
-                two.height += 30;
-                two.update()
-                };
-                
-               
-            })
-        };
-    
+//Add the element to the SVG canvas, resize and redraw canvas
+            two.add(text);
+            two.height += 30;
+            two.update();
+        }
+    });
 
-    getJsonData("")
-    
-    
-    function getFileInfo(id, callback){
-        $.getJSON('/dirstruct/'+id, function(json, textStatus){
-            callback(json)
-        });
+    socket.on("send_message", function(data){
+        console.log(data.message)
+    })
+
+    function ReadFileInfo(filename){
+        path = path + '/' + filename
+        console.log(path)
+        socket.emit("request_file_tree", {path:path})
     }
 
-
-    
-
 });
+
+
     
     
 
--- server.js
+++ server.js
@@ -5,32 +5,57 @@ var session = require('express-session');
 var path = require('path');
 var app = express();
 var root = __dirname
+app.use(express.static(root));
+app.use(session({secret: 'keyboard cat', cookie: { maxAge: 60000}, resave: true, saveUninitialized: true}));
 var server = app.listen(8000, function(){
     console.log("listening on port 8000\nPress Ctrl-C to stop.");
 });
 var io = require('socket.io').listen(server);
-app.use(express.static(root));
-app.use(session({secret: 'keyboard cat', cookie: { maxAge: 60000}, resave: true, saveUninitialized: true}));
 
 
-app.get('/dirstruct/', function(req, res){
+//Sockets code
+io.sockets.on('connection', function(socket){
+    console.log("Connected on socket ", socket.id);
     var mydir = getUserHome()
     var target = fs.readdirSync(mydir);
-    res.json(target);
-});
+    socket.emit("send_file_tree", {filetree:target})
 
-app.get('/dirstruct/:id', function(req, res){
-    var mydir = getUserHome() + '/' + req.params.id 
-    if(fs.statSync(mydir).isDirectory()){
-        console.log(req.params.id, req.body, "It's a directory")
-        res.json(fs.readdirSync(mydir));
-    }
-    else if(fs.statSync(mydir).isFile()){
-        console.log(req.params.id, req.body, "It's a file")
-        res.json({"message": "It's a file"})
-    }
-    var target = fs.readdirSync
+socket.on('request_file_tree', function(data){
+    var filePath = getUserHome() + data.path
+    if (fs.statSync(filePath).isDirectory()){
+        socket.emit("send_file_tree", {filetree:fs.readdirSync(filePath)})    
+        }
+    else {
+        socket.emit("send_message", {message:"It's a file!"})
+        }
+    })
+
+socket.on('disconnect', function(){
+        console.log('User disconnected')
+    })
 })
+
+
+
+// //HTTP code
+// app.get('/dirstruct/', function(req, res){
+//     var mydir = getUserHome()
+//     var target = fs.readdirSync(mydir);
+//     res.json(target);
+// });
+
+// app.get('/dirstruct/:id', function(req, res){
+//     var mydir = getUserHome() + '/' + req.params.id 
+//     if(fs.statSync(mydir).isDirectory()){
+//         console.log(req.params.id, req.body, "It's a directory")
+//         res.json(fs.readdirSync(mydir));
+//     }
+//     else if(fs.statSync(mydir).isFile()){
+//         console.log(req.params.id, req.body, "It's a file")
+//         res.json({"message": "It's a file"})
+//     }
+//     var target = fs.readdirSync
+// })
 
 function getUserHome() {
   return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
